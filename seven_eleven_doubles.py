f84496870d5a977ed04e27b3eff7bae165d4fc6d
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-27 11:15:51 -0400

    seven eleven doubles Part A

--- seven_eleven_doubles.py
+++ seven_eleven_doubles.py
@@ -1,5 +1,11 @@
+import random
+
 def roll_die(sides):
-    """ Complete this function with your solution for Part A """
+    """
+    This function returns a random number between 1 to the total amount of sides of a die
+    """
+    random_num = random.randint(1, sides)
+    return random_num
 
 def take_turn():
     """ Complete this function with your solution for Part C """
