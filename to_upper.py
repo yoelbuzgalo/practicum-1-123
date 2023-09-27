b69773b1830e342553a22406304515c859e65c08
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-27 11:50:58 -0400

    to_upper

--- to_upper.py
+++ to_upper.py
@@ -1,2 +1,14 @@
+START_VALUE = ord("A")
+
 def convert_to_upper(char):
-    """ Complete this function with your solution """
+    """ 
+    This function converts lower case characters to upper case characters
+    """
+    char_ascii_value = ord(char) - 65
+    selected_upper_case = chr(START_VALUE + char_ascii_value)
+    return selected_upper_case
+
+def main():
+    print(convert_to_upper("a"))
+
+main()
